4895bfca7e27f751804a59f066ba1794b49c783b
Aleks <bejbi994@gmail.com>
2023-05-18 17:50:42 +0200

    Crear-Reacr-App

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header >
+       <a href="/">BEST OF MARGOT</a>
+      </header>
+      <main>
+        List of products
+      </main>
+    </div>
+  );
+}
+
+export default App;
